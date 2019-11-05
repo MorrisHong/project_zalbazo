4e631939574b286b22fa03c64091c4e465fae4db
gracelove <govlmo91@gmail.com>
2019-11-06 01:07:42 +0900

    캐릭터인코딩설정

--- src/main/java/kr/project/zalbazo/config/WebConfig.java
+++ src/main/java/kr/project/zalbazo/config/WebConfig.java
@@ -1,6 +1,10 @@
 package kr.project.zalbazo.config;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
 
 public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
@@ -16,5 +20,11 @@ public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitiali
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter("UTF-8", true);
+        return new Filter[]{new HiddenHttpMethodFilter(), encodingFilter};
     }
 }
